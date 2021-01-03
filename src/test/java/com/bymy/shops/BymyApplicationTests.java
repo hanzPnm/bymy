b4731dc7114bcf795d1d6166b4de4aacdc224c14
hamza <alibihamza11@gmail.com>
2021-01-03 14:27:19 +0100

    first import

---
+++ src/test/java/com/bymy/shops/BymyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bymy.shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BymyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
