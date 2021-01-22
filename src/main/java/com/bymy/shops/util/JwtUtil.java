be5ed71e493d806ca89bac7d70d5c45e619ad75b
hamza <alibihamza11@gmail.com>
2021-01-22 22:05:08 +0100

    add role to jwt and extract role and username from jwt

--- src/main/java/com/bymy/shops/util/JwtUtil.java
+++ src/main/java/com/bymy/shops/util/JwtUtil.java
@@ -2,9 +2,14 @@ package com.bymy.shops.util;
 
 import java.util.Date;
 
+import com.bymy.shops.service.CustomUserDetailsService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
@@ -16,19 +21,32 @@ public class JwtUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
 
+    private static final String ROLES_CLAIM_NAME = "ROLES_CLAIM_NAME";
+
     @Value("${app.jwtSecret}")
     private String jwtSecret;
 
     @Value("${app.jwtExpirationInMs}")
     private int jwtExpirationInMs;
 
-    public String generateToken(String userName) {
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
 
+    public String generateToken(String userName) {
 
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
 
+        UserDetails user = customUserDetailsService.loadUserByUsername(userName);
+        String role = "";
+        if(null == user){
+            throw new UsernameNotFoundException(userName);
+        }else{
+            role = user.getAuthorities().toString();
+        }
+
         return Jwts.builder()
+                .claim(ROLES_CLAIM_NAME, role)
                 .setSubject(userName)
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
--- src/main/webapp/shopsfo/src/action/loginActions.js
+++ src/main/webapp/shopsfo/src/action/loginActions.js
@@ -5,11 +5,12 @@ import history from '../history';
 export const login = (cred) => (dispatch) => {
     UserService.login(cred).then(
     (res) => {
+        console.log(getUserFromToken(res.data.accessToken));
         dispatch({
             type: LOGIN,
             payload: res.data
         });
-        history.push('/');
+        //history.push('/');
         return Promise.resolve();
     },
     (error) => {
@@ -18,3 +19,19 @@ export const login = (cred) => (dispatch) => {
     }
 );
 }
+
+const getUserFromToken = token => {
+    if (token) {
+      try {
+            return {
+                userName : JSON.parse(atob(token.split('.')[1])).sub,
+                role : JSON.parse(atob(token.split('.')[1])).ROLES_CLAIM_NAME,
+                token: token
+            };
+      } catch (error) {
+        // ignore
+      }
+    }
+  
+    return null;
+  };
